6d5ece14ccc36c548d960d39d7ba787b5eff8683
George Middleton <georgemiddleton3@gmail.com>
2020-04-11 13:22:42 -0600

    Add basic Node test

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,22 +1,9 @@
 #include <catch2/catch.hpp>
 #include <rbtree.hpp>
 
-unsigned int Factorial(unsigned int number)
-{
-  return number <= 1 ? number : Factorial(number - 1) * number;
-}
-
-TEST_CASE("Factorials are computed", "[factorial]")
-{
-  REQUIRE(Factorial(1) == 1);
-  REQUIRE(Factorial(2) == 2);
-  REQUIRE(Factorial(3) == 6);
-  REQUIRE(Factorial(10) == 3628800);
-}
-
 TEST_CASE("Library is imported", "[library]")
 {
-  REQUIRE(rbt::add(1, 1) == 2);
-  REQUIRE(rbt::add(2, 2) == 4);
-  REQUIRE(rbt::add(1, 2) == 3);
+  rbt::Node test;
+  test.value = 5;
+  REQUIRE(test.value == 5);
 }
