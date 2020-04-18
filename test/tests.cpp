6b110a09fdc071b05529ccf8a9777e06ef1e3ff8
George Middleton <georgemiddleton3@gmail.com>
2020-04-17 23:45:11 -0600

    Update tests to insert with values

--- test/tests.cpp
+++ test/tests.cpp
@@ -5,41 +5,46 @@
 
 TEST_CASE("Library is imported", "[library]")
 {
-  rbt::Node<int> first{};
+  rbt::Node<int, std::string> first{};
   first.key = 5;
+  first.value = "Hello";
   REQUIRE(first.key == 5);
+  REQUIRE(first.value == "Hello");
 
-  rbt::Node<int> second{ 3, rbt::BLACK, nullptr, nullptr, &first };
+  rbt::Node<int, std::string> second{ 3, "World", rbt::BLACK, nullptr, nullptr, &first };
   REQUIRE(second.key == 3);
+  REQUIRE(second.value == "World");
   REQUIRE(second.parent == &first);
   first.left = &second;
   REQUIRE(first.left == &second);
   REQUIRE(first.left->key == 3);
 
-  rbt::RBTree<int> tree;
-  tree.Insert(3);
-  tree.Insert(2);
-  tree.Insert(4);
-  tree.Insert(10);
-  tree.Insert(7);
-  tree.Insert(1);
-  tree.Insert(5);
-  tree.Insert(20);
-  tree.Insert(2);
-  tree.Insert(3);
-  tree.Insert(6);
+  rbt::RBTree<int, char> tree;
+  tree.Insert(3, 'C');
+  tree.Insert(2, 'B');
+  tree.Insert(4, 'D');
+  tree.Insert(10, 'J');
+  tree.Insert(7, 'G');
+  tree.Insert(1, 'A');
+  tree.Insert(5, 'E');
+  tree.Insert(20, 'T');
+  tree.Insert(2, 'b');
+  tree.Insert(3, 'c');
+  tree.Insert(6, 'F');
   REQUIRE(tree.GetRoot()->key == 4);
+  REQUIRE(tree.GetRoot()->value == 'D');
   tree.Dump();
   tree.Remove(20);
   tree.Dump();
   tree.Remove(4);
   tree.Dump();
 
-  rbt::RBTree<std::string> stringTree;
-  stringTree.Insert("A");
-  stringTree.Insert("Apple");
-  stringTree.Insert("Bob");
+  rbt::RBTree<std::string, int> stringTree;
+  stringTree.Insert("A", 1);
+  stringTree.Insert("Apple", 2);
+  stringTree.Insert("Bob", 3);
   stringTree.Dump();
   stringTree.Remove("A");
   stringTree.Dump();
+  REQUIRE(stringTree.GetValueFromKey("Apple") == 2);
 }
